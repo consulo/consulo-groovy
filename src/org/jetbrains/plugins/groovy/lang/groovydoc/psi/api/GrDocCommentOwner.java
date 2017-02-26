cbdf38e50365f70926425b804dc96e05e88aae20
VISTALL <vistall.valeriy@gmail.com>
2017-02-26 19:13:53 +0300

    compilation

--- src/org/jetbrains/plugins/groovy/lang/groovydoc/psi/api/GrDocCommentOwner.java
+++ src/org/jetbrains/plugins/groovy/lang/groovydoc/psi/api/GrDocCommentOwner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,19 @@
 
 package org.jetbrains.plugins.groovy.lang.groovydoc.psi.api;
 
-import com.intellij.psi.PsiDocCommentOwner;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.groovy.lang.psi.GroovyPsiElement;
+import com.intellij.psi.PsiDocCommentOwner;
 
 /**
  * @author Maxim.Medvedev
  */
-public interface GrDocCommentOwner extends GroovyPsiElement, PsiDocCommentOwner {
-  @Nullable
-  GrDocComment getDocComment();
-  
-  boolean isDeprecated();  
+public interface GrDocCommentOwner extends GroovyPsiElement, PsiDocCommentOwner
+{
+	@Override
+	@Nullable
+	GrDocComment getDocComment();
+
+	@Override
+	boolean isDeprecated();
 }
