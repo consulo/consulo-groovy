7294a37859f8546115562ce53f1b60bf2327b622
VISTALL <vistall.valeriy@gmail.com>
2021-11-06 20:35:07 +0300

    drop annotationholder impl

--- groovy-impl/src/main/java/org/jetbrains/plugins/groovy/annotator/checkers/AliasedAnnotationHolder.java
+++ groovy-impl/src/main/java/org/jetbrains/plugins/groovy/annotator/checkers/AliasedAnnotationHolder.java
@@ -15,148 +15,35 @@
  */
 package org.jetbrains.plugins.groovy.annotator.checkers;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotation;
-import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.annotation.Annotation;
-import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.AnnotationSession;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.groovy.lang.psi.api.auxiliary.modifiers.annotation.GrAnnotation;
+import org.jetbrains.plugins.groovy.lang.psi.api.types.GrCodeReferenceElement;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Created by Max Medvedev on 25/03/14
  */
-class AliasedAnnotationHolder implements AnnotationHolder
+class AliasedAnnotationHolder
 {
-	private final AnnotationHolder myHolder;
-	private final GrAnnotation myAlias;
-	private final GrCodeReferenceElement myReference;
-
-	public AliasedAnnotationHolder(@Nonnull AnnotationHolder holder, @Nonnull GrAnnotation alias)
-	{
-		myHolder = holder;
-		myAlias = alias;
-		myReference = myAlias.getClassReference();
-	}
-
 	@Nonnull
-	private PsiElement findCodeElement(@Nonnull PsiElement elt)
+	static PsiElement findCodeElement(@Nonnull PsiElement elt, @Nonnull GrAnnotation alias, @Nonnull GrCodeReferenceElement aliasReference)
 	{
-		if(PsiTreeUtil.isAncestor(myAlias, elt, true))
+		if(PsiTreeUtil.isAncestor(alias, elt, true))
 		{
 			return elt;
 		}
 		else
 		{
-			return myReference;
+			return aliasReference;
 		}
-	}
-
-	@Override
-	public Annotation createErrorAnnotation(@Nonnull PsiElement elt, @Nullable String message)
-	{
-		PsiElement codeElement = findCodeElement(elt);
-		return myHolder.createErrorAnnotation(codeElement, message);
-	}
-
-	@Override
-	public Annotation createErrorAnnotation(@Nonnull ASTNode node, @Nullable String message)
-	{
-		return createErrorAnnotation(node.getPsi(), message);
-	}
-
-	@Override
-	public Annotation createErrorAnnotation(@Nonnull TextRange range, @javax.annotation.Nullable String message)
-	{
-		throw new UnsupportedOperationException("unsupported");
-	}
-
-	@Override
-	public Annotation createWarningAnnotation(@Nonnull PsiElement elt, @Nullable String message)
-	{
-		return myHolder.createWarningAnnotation(findCodeElement(elt), message);
-	}
-
-	@Override
-	public Annotation createWarningAnnotation(@Nonnull ASTNode node, @javax.annotation.Nullable String message)
-	{
-		return myHolder.createWarningAnnotation(node.getPsi(), message);
-	}
-
-	@Override
-	public Annotation createWarningAnnotation(@Nonnull TextRange range, @Nullable String message)
-	{
-		throw new UnsupportedOperationException("unsupported");
-	}
-
-	@Override
-	public Annotation createWeakWarningAnnotation(@Nonnull PsiElement elt, @javax.annotation.Nullable String message)
-	{
-		return myHolder.createWeakWarningAnnotation(findCodeElement(elt), message);
-	}
-
-	@Override
-	public Annotation createWeakWarningAnnotation(@Nonnull ASTNode node, @Nullable String message)
-	{
-		return myHolder.createWarningAnnotation(node.getPsi(), message);
-	}
-
-	@Override
-	public Annotation createWeakWarningAnnotation(@Nonnull TextRange range, @Nullable String message)
-	{
-		throw new UnsupportedOperationException("unsupported");
-	}
-
-	@Override
-	public Annotation createInfoAnnotation(@Nonnull PsiElement elt, @Nullable String message)
-	{
-		return myHolder.createInfoAnnotation(findCodeElement(elt), message);
-	}
-
-	@Override
-	public Annotation createInfoAnnotation(@Nonnull ASTNode node, @Nullable String message)
-	{
-		return myHolder.createInfoAnnotation(node.getPsi(), message);
-	}
-
-	@Override
-	public Annotation createInfoAnnotation(@Nonnull TextRange range, @javax.annotation.Nullable String message)
-	{
-		throw new UnsupportedOperationException("unsupported");
-	}
-
-	@Override
-	public Annotation createAnnotation(@Nonnull HighlightSeverity severity,
-			@Nonnull TextRange range,
-			@Nullable String message)
-	{
-		throw new UnsupportedOperationException("unsupported");
-	}
-
-	/*@Override
-	public Annotation createAnnotation(@NotNull HighlightSeverity severity,
-			@NotNull TextRange range,
-			@Nullable String message,
-			@Nullable String htmlTooltip)
-	{
-		throw new UnsupportedOperationException("unsupported");
-	}  */
-
-	@Nonnull
-	@Override
-	public AnnotationSession getCurrentAnnotationSession()
-	{
-		return myHolder.getCurrentAnnotationSession();
-	}
-
-	@Override
-	public boolean isBatchMode()
-	{
-		return myHolder.isBatchMode();
 	}
 }
