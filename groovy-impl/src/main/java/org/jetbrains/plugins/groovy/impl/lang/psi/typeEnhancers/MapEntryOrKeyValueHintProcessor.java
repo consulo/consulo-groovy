70126e6a6fd2d5c831ecc41a3b303ce9d4e31086
Nikolay Yurchenko <nikolay@yurchenko.su>
2026-02-19 22:34:39 +0300

    Replacing deprecated consulo.ide.impl.idea.openapi.util.text.StringUtil with consulo.util.lang.StringUtil. Some refactoring. (#61)

--- groovy-impl/src/main/java/org/jetbrains/plugins/groovy/impl/lang/psi/typeEnhancers/MapEntryOrKeyValueHintProcessor.java
+++ groovy-impl/src/main/java/org/jetbrains/plugins/groovy/impl/lang/psi/typeEnhancers/MapEntryOrKeyValueHintProcessor.java
@@ -15,122 +15,134 @@
  */
 package org.jetbrains.plugins.groovy.impl.lang.psi.typeEnhancers;
 
-import consulo.ide.impl.idea.openapi.util.text.StringUtilRt;
-import consulo.util.lang.Couple;
 import com.intellij.java.language.psi.*;
 import com.intellij.java.language.psi.util.InheritanceUtil;
 import com.intellij.java.language.psi.util.PsiUtil;
 import consulo.util.collection.ContainerUtil;
+import consulo.util.lang.Couple;
+import consulo.util.lang.StringUtil;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 
 import java.util.List;
 
 /**
- * Created by Max Medvedev on 28/02/14
+ * @author Max Medvedev
+ * @since 2014-02-28
  */
 public class MapEntryOrKeyValueHintProcessor extends SignatureHintProcessor {
-  @Override
-  public String getHintName() {
-    return "groovy.transform.stc.MapEntryOrKeyValue";
-  }
-
-  @Nonnull
-  @Override
-  public List<PsiType[]> inferExpectedSignatures(@Nonnull PsiMethod method,
-                                                 @Nonnull PsiSubstitutor substitutor,
-                                                 @Nonnull String[] options) {
-    int argNum = extractArgNum(options);
-    boolean index = extractIndex(options);
-
-    PsiParameter[] parameters = method.getParameterList().getParameters();
-
-    if (argNum >= parameters.length) return List.of();
-
-    PsiParameter parameter = parameters[argNum];
-    PsiType type = parameter.getType();
-    PsiType substituted = substitutor.substitute(type);
-
-    if (!InheritanceUtil.isInheritor(substituted, CommonClassNames.JAVA_UTIL_MAP)) return List.of();
-
-    PsiType key = PsiUtil.substituteTypeParameter(substituted, CommonClassNames.JAVA_UTIL_MAP, 0, true);
-    PsiType value = PsiUtil.substituteTypeParameter(substituted, CommonClassNames.JAVA_UTIL_MAP, 1, true);
-
-    PsiClass mapEntry = JavaPsiFacade.getInstance(method.getProject()).findClass(CommonClassNames.JAVA_UTIL_MAP_ENTRY, method.getResolveScope());
-    if (mapEntry == null) return List.of();
-
-    PsiClassType mapEntryType = JavaPsiFacade.getElementFactory(method.getProject()).createType(mapEntry, key, value);
-
-    PsiType[] keyValueSignature = index ? new PsiType[]{key, value, PsiType.INT} : new PsiType[]{key, value};
-    PsiType[] mapEntrySignature = index ? new PsiType[]{mapEntryType, PsiType.INT} : new PsiType[]{mapEntryType};
-
-    return ContainerUtil.newArrayList(keyValueSignature, mapEntrySignature);
-  }
-
-  private static int extractArgNum(String[] options) {
-
-
-    for (String value : options) {
-      Integer parsedValue = parseArgNum(value);
-      if (parsedValue != null) {
-        return parsedValue;
-      }
+    @Override
+    public String getHintName() {
+        return "groovy.transform.stc.MapEntryOrKeyValue";
     }
 
-    if (options.length == 1) {
-      return StringUtilRt.parseInt(options[0], 0);
+    @Nonnull
+    @Override
+    public List<PsiType[]> inferExpectedSignatures(
+        @Nonnull PsiMethod method,
+        @Nonnull PsiSubstitutor substitutor,
+        @Nonnull String[] options
+    ) {
+        int argNum = extractArgNum(options);
+        boolean index = extractIndex(options);
+
+        PsiParameter[] parameters = method.getParameterList().getParameters();
+
+        if (argNum >= parameters.length) {
+            return List.of();
+        }
+
+        PsiParameter parameter = parameters[argNum];
+        PsiType type = parameter.getType();
+        PsiType substituted = substitutor.substitute(type);
+
+        if (!InheritanceUtil.isInheritor(substituted, CommonClassNames.JAVA_UTIL_MAP)) {
+            return List.of();
+        }
+
+        PsiType key = PsiUtil.substituteTypeParameter(substituted, CommonClassNames.JAVA_UTIL_MAP, 0, true);
+        PsiType value = PsiUtil.substituteTypeParameter(substituted, CommonClassNames.JAVA_UTIL_MAP, 1, true);
+
+        PsiClass mapEntry =
+            JavaPsiFacade.getInstance(method.getProject()).findClass(CommonClassNames.JAVA_UTIL_MAP_ENTRY, method.getResolveScope());
+        if (mapEntry == null) {
+            return List.of();
+        }
+
+        PsiClassType mapEntryType = JavaPsiFacade.getElementFactory(method.getProject()).createType(mapEntry, key, value);
+
+        PsiType[] keyValueSignature = index ? new PsiType[]{key, value, PsiType.INT} : new PsiType[]{key, value};
+        PsiType[] mapEntrySignature = index ? new PsiType[]{mapEntryType, PsiType.INT} : new PsiType[]{mapEntryType};
+
+        return ContainerUtil.newArrayList(keyValueSignature, mapEntrySignature);
     }
 
-    return 0;
-  }
+    private static int extractArgNum(String[] options) {
+        for (String value : options) {
+            Integer parsedValue = parseArgNum(value);
+            if (parsedValue != null) {
+                return parsedValue;
+            }
+        }
 
-  private static boolean extractIndex(String[] options) {
-    for (String value : options) {
-      Boolean parsedValue = parseIndex(value);
-      if (parsedValue != null) {
-        return parsedValue;
-      }
+        if (options.length == 1) {
+            return StringUtil.parseInt(options[0], 0);
+        }
+
+        return 0;
     }
 
-    if (options.length == 1) {
-      return StringUtilRt.parseBoolean(options[0], false);
+    private static boolean extractIndex(String[] options) {
+        for (String value : options) {
+            Boolean parsedValue = parseIndex(value);
+            if (parsedValue != null) {
+                return parsedValue;
+            }
+        }
+
+        if (options.length == 1) {
+            return StringUtil.parseBoolean(options[0], false);
+        }
+
+        return false;
     }
 
-    return false;
-  }
+    private static Boolean parseIndex(String value) {
+        Couple<String> pair = parseValue(value);
+        if (pair == null) {
+            return null;
+        }
 
-  private static Boolean parseIndex(String value) {
-    Couple<String> pair = parseValue(value);
-    if (pair == null) return null;
+        Boolean parsedValue = StringUtil.parseBoolean(pair.getSecond(), false);
+        if ("index".equals(pair.getFirst())) {
+            return parsedValue;
+        }
 
-    Boolean parsedValue = StringUtilRt.parseBoolean(pair.getSecond(), false);
-    if ("index".equals(pair.getFirst())) {
-      return parsedValue;
+        return null;
     }
 
-    return null;
-  }
+    private static Integer parseArgNum(String value) {
+        Couple<String> pair = parseValue(value);
+        if (pair == null) {
+            return null;
+        }
 
-  private static Integer parseArgNum(String value) {
-    Couple<String> pair = parseValue(value);
-    if (pair == null) return null;
+        Integer parsedValue = StringUtil.parseInt(pair.getSecond(), 0);
+        if ("argNum".equals(pair.getFirst())) {
+            return parsedValue;
+        }
 
-    Integer parsedValue = StringUtilRt.parseInt(pair.getSecond(), 0);
-    if ("argNum".equals(pair.getFirst())) {
-      return parsedValue;
+        return null;
     }
 
-    return null;
-  }
+    @Nullable
+    private static Couple<String> parseValue(String value) {
+        String[] splitted = value.split("=");
 
-  @Nullable
-  private static Couple<String> parseValue(String value) {
-    String[] splitted = value.split("=");
+        if (splitted.length == 2) {
+            return Couple.of(splitted[0].trim(), splitted[1].trim());
+        }
 
-    if (splitted.length == 2) {
-      return Couple.of(splitted[0].trim(), splitted[1].trim());
+        return null;
     }
-
-    return null;
-  }
 }
